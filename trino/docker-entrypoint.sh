c16c5ed8d608dd2745364d649588797ce74f137c
Aakash Nand <14219201+aakashnand@users.noreply.github.com>
2022-06-07 01:45:35 +0900

    Update trino-ranger-plugin from v2.1.0 to v2.3.0
    Improves ranger-admin docker image by adding the downloadable file to
    docker image. This will save time when restarting the containers

--- trino/docker-entrypoint.sh
+++ trino/docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-RANGER_VERSION=3.0.0-SNAPSHOT # Supported values 2.1.0 and 3.0.0-SNAPSHOT
+RANGER_VERSION=2.3.0
 cd /root && \
 curl https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/ranger-${RANGER_VERSION}-trino-plugin.tar.gz --output ranger-${RANGER_VERSION}-trino-plugin.tar.gz -L && 
 tar xvf ranger-${RANGER_VERSION}-trino-plugin.tar.gz && \
-yes | cp -rf /root/enable-trino-plugin.sh /root/ranger-${RANGER_VERSION}-trino-plugin/ && \
 yes | cp -rf /root/install.properties /root/ranger-${RANGER_VERSION}-trino-plugin/ && \
 chown root:root -R /root/ranger-${RANGER_VERSION}-trino-plugin/* && \
 /root/ranger-${RANGER_VERSION}-trino-plugin/enable-trino-plugin.sh && \ 
