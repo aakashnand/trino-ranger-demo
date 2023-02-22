b4eacb021690995b7bbc032a3990005a0f7c47ad
Aakash Nand <aakashnand@gmail.com>
2023-02-22 20:57:12 +0900

    Update trino version to 406

--- trino/docker-entrypoint.sh
+++ trino/docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-RANGER_VERSION=2.3.0
+RANGER_VERSION=ranger-3.0.0-SNAPSHOT-trino-plugin-405-406
 cd /root && \
-curl https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/ranger-${RANGER_VERSION}-trino-plugin.tar.gz --output ranger-${RANGER_VERSION}-trino-plugin.tar.gz -L && 
-tar xvf ranger-${RANGER_VERSION}-trino-plugin.tar.gz && \
-yes | cp -rf /root/install.properties /root/ranger-${RANGER_VERSION}-trino-plugin/ && \
-chown root:root -R /root/ranger-${RANGER_VERSION}-trino-plugin/* && \
-/root/ranger-${RANGER_VERSION}-trino-plugin/enable-trino-plugin.sh && \ 
+curl https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/${RANGER_VERSION}.tar.gz --output ${RANGER_VERSION}.tar.gz -L && 
+mkdir -p /root/${RANGER_VERSION} && \
+tar xvf ${RANGER_VERSION}.tar.gz -C /root/${RANGER_VERSION} --strip-components 1 && \
+yes | cp -rf /root/install.properties /root/${RANGER_VERSION}/ && \
+chown root:root -R /root/${RANGER_VERSION}/* && \
+/root/${RANGER_VERSION}/enable-trino-plugin.sh && \
 /usr/lib/trino/bin/run-trino
