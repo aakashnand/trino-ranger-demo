c42909aa326bbca827858bcddc95a32a89b0324c
Aakash Nand <14219201+aakashnand@users.noreply.github.com>
2024-01-29 17:26:51 +0900

    Upgrade trino to 436 and docker image to v4

--- trino/docker-entrypoint.sh
+++ trino/docker-entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-RANGER_VERSION=ranger-3.0.0-SNAPSHOT-trino-plugin-405-406
-cd /root && \
-curl https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/${RANGER_VERSION}.tar.gz --output ${RANGER_VERSION}.tar.gz -L && 
+microdnf install gzip -y
+RANGER_VERSION=ranger-3.0.0-SNAPSHOT-trino-plugin-436
+TAR_FILE=/root/${RANGER_VERSION}.tar.gz
+cd /root
+# Check if the tar file exists
+if [ -f "$TAR_FILE" ]; then
+    echo "Tar file $TAR_FILE already exists."
+else
+    echo "Tar file $TAR_FILE does not exist. Downloading from $DOWNLOAD_URL..."
+    curl -k https://github.com/aakashnand/trino-ranger-demo/releases/download/trino-ranger-demo-v1.0/${RANGER_VERSION}.tar.gz --output ${RANGER_VERSION}.tar.gz -L
+    echo "Download completed."
+fi
+
 mkdir -p /root/${RANGER_VERSION} && \
 tar xvf ${RANGER_VERSION}.tar.gz -C /root/${RANGER_VERSION} --strip-components 1 && \
 yes | cp -rf /root/install.properties /root/${RANGER_VERSION}/ && \
